ebb28ac72dd83e9211425d64abb495b5ba46b1e6
luisillobret2 <107351299+luisillobret2@users.noreply.github.com>
2023-01-27 10:16:57 +0100

    Initial Commit

---
+++ simpleApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  simpleApp
+//
+//  Created by Luis Bretones Hernandez on 27/1/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ simpleApp/simpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  simpleAppApp.swift
+//  simpleApp
+//
+//  Created by Luis Bretones Hernandez on 27/1/23.
+//
+
+import SwiftUI
+
+@main
+struct simpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
